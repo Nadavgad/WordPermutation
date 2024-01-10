f66ef5208a4e05839986560a0f2aef570f180b44
Nadav Gad <35539676+Nadavgad@users.noreply.github.com>
2024-01-10 11:29:52 +0200

    Update WordPermutation.py

--- WordPermutation.py
+++ WordPermutation.py
@@ -5,23 +5,18 @@ def is_permutation(string1, string2):
     :return: if string1 and string2 are permutation
     """
 
-    # Check if the lengths of the two strings are different
     if len(string1) != len(string2):
         return False
 
-    # Initialize dictionaries to store character counts for each string
     char_counter_string1 = {}
     char_counter_string2 = {}
 
-    # Count character occurrences in string1
     for char in string1:
         char_counter_string1[char] = char_counter_string1.get(char, 0) + 1
 
-    # Count character occurrences in string2
     for char in string2:
         char_counter_string2[char] = char_counter_string2.get(char, 0) + 1
 
-    # Check if the character counts are the same for both strings
     return char_counter_string1 == char_counter_string2
 
 
