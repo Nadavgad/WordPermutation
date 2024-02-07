5053723d154ed57d75a4964c1b7d263df84e2a83
Nadav Gad <35539676+Nadavgad@users.noreply.github.com>
2024-02-07 15:02:33 +0200

    Update WordPermutation.py

--- WordPermutation.py
+++ WordPermutation.py
@@ -1,9 +1,11 @@
 def is_permutation(string1, string2):
+
     """
     :param string1: first string
     :param string2: second string
     :return: if string1 and string2 are permutation
     """
+
     if len(string1) != len(string2):
         return False
 
@@ -17,8 +19,3 @@ def is_permutation(string1, string2):
         char_counter_string2[char] = char_counter_string2.get(char, 0) + 1
 
     return char_counter_string1 == char_counter_string2
-
-
-# Test
-result = is_permutation("haawbsc", "bsaawch")
-print(F"\nResult ---> {result}")
